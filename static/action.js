4ef6f6fb5f22d715e29e6bdaf1320974e8b86d19
elijah <soniceelijah@yandex.ru>
2022-03-11 19:38:24 +0300

    touch scroll

--- static/action.js
+++ static/action.js
@@ -36,6 +36,52 @@ function move(dir) {
             frame.removeAttribute('scrolling');
         }
     }, 200);
-
-
 }
+
+// Нагло стырил со stackoverflow
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+        } else {
+            /* left swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            move(1);
+        } else { 
+            move(-1);
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
